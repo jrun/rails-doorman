851b783fe76e1c2a6bb5b7beebe83687f701ebad
Jeremy Burks <jeremy.burks@gmail.com>
2009-05-25 09:28:33 -0500

    removed old commented out code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,17 +13,3 @@ require 'rails_doorman'
 
 Spec::Runner.configure do |config|
 end
-
-# # deny :user => "bill"       # calls current_user.login, but this is configurable
-# # deny {|c| c.params["arbitrary"] == "expressions"}
-# #
-# #
-# # # mostly closed:
-# # deny :all                           # removes implicit final allow :all
-# # allow :host => "*.example.com"
-# # allow :time => "8am-5pm"
-# #
-# # # store a block for repeated usage
-# # Merb::Access.add_block :admin, {|c| c.current_user.admin?}
-# #
-# # allow :admin
